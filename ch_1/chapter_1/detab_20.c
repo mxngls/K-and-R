b384b6b658473b94b3f849e5b925f47c43007c5f
mxngls <Maximilian.e.hoenig@gmail.com>
2023-09-06 07:38:02 +0900

    Rename files and restructure directory

--- ch_1/chapter_1/detab_20.c
+++ ch_1/chapter_1/detab_20.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
-#define TABSTOP 8
+#define TABSTOP 5
 
 int main() {
-  int c, i;
+  int c, i, off;
 
   while ((c = getchar()) != EOF) {
     if (c == '\t') {
-      i = TABSTOP;
+      i = TABSTOP - (off % TABSTOP);
       while (i-- > 0) {
         putchar(' ');
+        off++;
       }
+
     } else {
       putchar(c);
+
+      if (c == '\n')
+        off = 0;
+      else
+        off++;
     }
   }
 
