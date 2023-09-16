9b84e775dd9d01d57911fa96af71d4a085acd31a
mxngls <Maximilian.e.hoenig@gmail.com>
2023-09-16 10:31:29 +0900

    Refactor solution for 1.23 of K&R
    Tried to use some more features introduced in the first chapter:
    
    * Global (external variables)
    * More function calls
    
    Overall the code seems to be more readable, but got kinda verbose as the
    logic that make up distinct functions is often kinda similar.

--- chapter_1/remove_comments_23.c
+++ chapter_1/remove_comments_23.c
@@ -8,22 +8,74 @@
 /* Don't forget to handle quoted strings and character constants properly. */
 /* C comments don't nest. */
 
-int get_line(char current_line[]);
+int string;
+int comment;
+int whitespace;
+
+int inspect_line(char line[]);
+int is_start_of_string(int i, char c, char line[]);
+int is_start_of_string(int i, char c, char line[]);
+int is_start_of_comment(int start, int i, char c, char line[]);
+int is_start_of_comment(int start, int i, char c, char line[]);
+int is_whitespace(int i, char c);
 
 int main() {
 
-  int l;
   char line[MAXLINE];
 
-  while ((l = get_line(line)) != -1) {
+  while (inspect_line(line) != -1) {
   }
 
   return 0;
 }
 
-int get_line(char line[]) {
+int is_start_of_string(int i, char c, char line[]) {
+  if (c == '"' && string == FALSE && line[i - 1] != '\\' && comment == FALSE)
+    return TRUE;
+  else
+    return FALSE;
+}
+
+int is_end_of_string(int i, char c, char line[]) {
+  if (c == '"' && string == TRUE && line[i - 1] != '\\' && comment == FALSE)
+    return TRUE;
+  else
+    return FALSE;
+}
+
+int is_start_of_comment(int start, int i, char c, char line[]) {
+  if (string == FALSE && i >= 1 && c == '*' && line[i - 1] == '/')
+    return TRUE;
+  else
+    return FALSE;
+}
+
+int is_end_of_comment(int start, int i, char c, char line[]) {
+  if (string == FALSE && i >= 1 && c == '/' && line[i - 1] == '*')
+    return FALSE;
+  else
+    return FALSE;
+}
+
+int is_whitespace(int i, char c) {
+  /* The *possible* start of a comment can't count as a non-whitespace */
+  /* character. So can't empty newlines or any characters inside a comment. */
+  if ((c != '/') && ((i == 0 && c == '\n') || (comment == FALSE && c != '\t' &&
+                                               c != ' ' && c != '\n'))) {
+    return FALSE;
+  } else
+    return TRUE;
+}
+
+char test[] = "\\* This is not a comment! *\\";
+
+int inspect_line(char line[]) {
   int c;
-  int i = 0, start = 0, string = FALSE, comment = FALSE, whitespace = TRUE;
+  int i = 0, start = 0;
+
+  string = FALSE;
+  comment = FALSE;
+  whitespace = TRUE;
 
   while ((c = getchar())) {
     /* Return if we reached the end of a file */
@@ -31,29 +83,23 @@ int get_line(char line[]) {
       return -1;
 
     line[i] = c;
- 
-    /* Check if the current column resides inside a string literal */
-    if (c == '"' && string == FALSE && comment == FALSE)
+
+    if (is_start_of_string(i, c, line))
       string = TRUE;
-    else if (c == '"' && string == TRUE && comment == FALSE)
+    if (is_end_of_string(i, c, line))
       string = FALSE;
 
-    /* Reset the cursor when a valid comment was found */
-    if (string == FALSE && i >= 1 && c == '*' && line[i - 1] == '/') {
-      start = i;
+    if (is_start_of_comment(start, i, c, line)) {
       comment = TRUE;
-    } else if (string == FALSE && i >= 1 && c == '/' && line[i - 1] == '*') {
-      i = start;
+      start = i;
+    }
+    if (is_end_of_comment(start, i, c, line)) {
       comment = FALSE;
+      i = start;
     }
 
-    /* The *possible* start of a comment can't count as a non-whitespace */
-    /* character. So can't empty newlines or any characters inside a comment. */
-    if ((c != '/') &&
-        ((i == 0 && c == '\n') ||
-         (comment == FALSE && c != '\t' && c != ' ' && c != '\n'))) {
+    if (!is_whitespace(i, c))
       whitespace = FALSE;
-    }
 
     /* Or the line */
     /* "This is a string inside a comment" */
