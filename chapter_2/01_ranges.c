381aa0add7fe3650ae3d16efe8d5f5efb1fd7832
mxngls <Maximilian.e.hoenig@gmail.com>
2023-12-12 22:29:52 +0900

    Format exercise 2-1

--- chapter_2/01_ranges.c
+++ chapter_2/01_ranges.c
@@ -11,6 +11,7 @@
 int main() {
 
   printf("Unsigned:\n");
+
   printf("%-10s: %20d ~ %20u\n", "Characters", 0, UCHAR_MAX);
   printf("%-10s: %20d ~ %20d\n", "Shorts", 0, USHRT_MAX);
   printf("%-10s: %20d ~ %20d\n", "Integers", 0, UINT_MAX);
@@ -19,6 +20,7 @@ int main() {
   printf("\n-------------------------------------------------------\n\n");
 
   printf("Signed:\n");
+
   printf("%-10s: %20d ~ %19d\n", "Characters", CHAR_MIN, CHAR_MAX);
   printf("%-10s: %20d ~ %19d\n", "Shorts", SHRT_MIN, SHRT_MAX);
   printf("%-10s: %20d ~ %19d\n", "Integers", INT_MIN, INT_MAX);
@@ -36,25 +38,28 @@ int main() {
    * (<type>) casting it the required type again
    *
    */
+
   printf("CALCULATED Unsigned:\n");
-  printf("%-10s: %20d ~ %20u\n", "Characters", 0,
-         ((unsigned char)~0));
-  printf("%-10s: %20d ~ %20u\n", "Shorts", 0,
-         ((unsigned short)~0));
-  printf("%-10s: %20d ~ %20u\n", "Integers", 0,
-         ((unsigned int)~0));
-  printf("%-10s: %20d ~ %20lu\n", "Longs", 1,
-         ((unsigned long)~0));
+
+  printf("%-10s: %20d ~ %20u\n", "Characters", 0, ((unsigned char)~0));
+  printf("%-10s: %20d ~ %20u\n", "Shorts", 0, ((unsigned short)~0));
+  printf("%-10s: %20d ~ %20u\n", "Integers", 0, ((unsigned int)~0));
+  printf("%-10s: %20d ~ %20lu\n", "Longs", 1, ((unsigned long)~0));
 
   printf("\n-------------------------------------------------------\n\n");
 
   printf("CALCULATED Signed:\n");
-  printf("%-10s: %20d ~ %20u\n", "Characters", -(char)((unsigned char)~0 >> 1) - 1,
-         (char)((unsigned char)~0 >> 1));
-  printf("%-10s: %20d ~ %20u\n", "Shorts", -(short)((unsigned short)~0 >> 1) - 1,
+
+  printf("%-10s: %20d ~ %20u\n", "Characters",
+         -(char)((unsigned char)~0 >> 1) - 1, (char)((unsigned char)~0 >> 1));
+
+  printf("%-10s: %20d ~ %20u\n", "Shorts",
+         -(short)((unsigned short)~0 >> 1) - 1,
          (short)((unsigned short)~0 >> 1));
+
   printf("%-10s: %20d ~ %20u\n", "Ints", -(int)((unsigned int)~0 >> 1) - 1,
          (int)((unsigned int)~0 >> 1));
+
   printf("%-10s: %20lu ~ %20lu\n", "Longs", -(long)((unsigned long)~0 >> 1) - 1,
          (long)((unsigned long)~0 >> 1));
 
