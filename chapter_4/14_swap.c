d04ee90576d67e57bf6f9213e1e7805d2e769680
mxngls <Maximilian.e.hoenig@gmail.com>
2024-07-04 00:08:13 +0900

    Fix solution 4-14

--- chapter_4/14_swap.c
+++ chapter_4/14_swap.c
@@ -2,20 +2,30 @@
 
 #define swap(t,x,y) { \
   t _z;               \
+  _z = x;             \
   x = y;              \
   y = _z;             \
 }
 
 int main() {
   int x, y;
-  
+  double a, b;
+
   x = 0;
   y = 1;
+
+  a = 0.00;
+  b = 1.11;
 
   swap(int,x,y);
 
   printf("Value of x equals: %d\n", x);
   printf("Value of y equals: %d\n", y);
+
+  swap(double,a,b);
+
+  printf("Value of a equals: %f\n", a);
+  printf("Value of b equals: %f\n", b);
 
   return 0;
 }
