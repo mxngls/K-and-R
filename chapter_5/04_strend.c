a6f263684d7e44a3680ffb923f5396e725ef3d04
mxngls <Maximilian.e.hoenig@gmail.com>
2024-07-05 22:23:14 +0900

    Add solution for exercise 5-4

---
+++ chapter_5/04_strend.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define SIZE 100
+
+int strend(char *s, char *t) {
+  char *c = t;
+  int f; /* indicate if a match for the first char of t was found */
+
+  f = 0;
+  while (*s) {
+    if (*s == *t) {
+      *c++ = *t++;
+      f = 1;
+    } else if (f) /* non-matching char found. return */
+      return 0;
+    s++;
+  }
+
+  return 1;
+}
+
+/*
+ * Exercise 5-4. Write the function strend(s,t), which returns 1 if the
+ * string t occurs at the end of the string s, and zero otherwise.
+ */
+int main() {
+  char s[SIZE] = "Hello, World!";
+  char t[SIZE] = "World!";
+
+  /* match */
+  printf("%d\n", strend(s, t));
+
+  char d[SIZE] = "Hello, World!";
+  char c[SIZE] = "Wrld!";
+
+  /* match */
+  printf("%d\n", strend(d, c));
+
+  return 0;
+}
