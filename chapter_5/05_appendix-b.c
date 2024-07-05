9d90eca54a00de5b203bc42cab7bf919c4a329a8
mxngls <Maximilian.e.hoenig@gmail.com>
2024-07-05 23:28:36 +0900

    Pad strncpy of solution 5-5 with null bytes

--- chapter_5/05_appendix-b.c
+++ chapter_5/05_appendix-b.c
@@ -13,25 +13,38 @@
 
 /*
  * copy at most n characters of string ct to s; return s.
- * Pad with '\0''s if ct has fewer than n characters.
+ * Pad with '\0's if ct has fewer than n characters.
  */
 char *_strncpy(char *s, const char *ct, size_t n) {
   int i;
 
   i = 0;
-  while (i++ < n && (*s++ = *ct++))
-    ;
+  while (i++ < n) {
+    if (*ct)
+      *s++ = *ct++;
+    else
+      *s++ = '\0';
+  }
 
   return s;
 }
 
 int main() {
   char source[SIZE] = "Hello, World!";
-  char target[SIZE];
+  char target_1[SIZE];
+  char target_2[SIZE];
 
-  _strncpy(target, source, 6);
+  _strncpy(target_1, source, 6);
 
-  printf("%s\n", target);
+  /* prints "Hello," */
+  printf("%s\n", target_1);
+
+  _strncpy(target_2, source, 20);
+
+  /* visualize null bytes used as padding */
+  int i = -1;
+  while (source[++i])
+    printf("%c\n", target_1[i]);
 
   return 0;
 }
