1185b40841d615cf03da2fdf1297a47d2b8dfaae
mxngls <Maximilian.e.hoenig@gmail.com>
2024-07-06 14:12:15 +0900

    Add strindex to solution for exercise 5-6

--- chapter_5/06_functions.c
+++ chapter_5/06_functions.c
@@ -16,6 +16,25 @@ int _getline(char *s, int lim) {
 
   return i;
 }
+
+/* strindex: return index of t in s, -1 if none */
+int _strindex(char *s, char *t) {
+  int i, j;
+
+  i = 0;
+  j = 0;
+  while (*s && *t) {
+    if (*s++ == *t) {
+      if (!j)
+        j = i;
+      t++;
+    } else if (j) {
+      return -1;
+    }
+    i++;
+  }
+
+  return j;
 }
 
 int main() {
@@ -24,5 +43,20 @@ int main() {
   while (_getline(s, SIZE) > 0)
     printf("Input: %s", s);
 
+  int i;
+
+  char a[SIZE] = "Hello, World!";
+  char b[SIZE] = "World";
+  i = _strindex(a, b);
+
+  /* prints "8" */
+  printf("%d \n", i);
+
+  char c[SIZE] = "Hello, World!";
+  char d[SIZE] = "Wod";
+  i = _strindex(c, d);
+
+  /* prints "-1" */
+  printf("%d \n", i);
   return 0;
 }
