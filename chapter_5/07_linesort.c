c20c6a638cc5490cdb790ff82d761a61dac1d2d0
mxngls <Maximilian.e.hoenig@gmail.com>
2024-07-07 17:22:33 +0900

    Finalize solution for exercise 5-7

--- chapter_5/07_linesort.c
+++ chapter_5/07_linesort.c
@@ -2,10 +2,11 @@
 #include <string.h>
 
 #define MAXLINES 5000 /* max #lines to be sorted */
+#define MAXLINE 1000  /* max chars per line */
 
-char *lineptr[MAXLINES]; /* pointers to text lines */
+char *lineptr[MAXLINES];         /* pointers to text lines */
 
-int readlines(char *ineptr[], int nlines);
+int readlines(char *ineptr[], char lines[], int nlines);
 void writelines(char *lineptr[], int nlines);
 
 void qsort(char *lnieptr[], int left, int right);
@@ -17,8 +18,9 @@ void qsort(char *lnieptr[], int left, int right);
  */
 int main() {
   int nlines;
+  char lines[MAXLINES * MAXLINE];
 
-  if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+  if ((nlines = readlines(lineptr, lines, MAXLINES)) >= 0) {
     qsort(lineptr, 0, nlines - 1);
     writelines(lineptr, nlines);
     return 0;
@@ -43,33 +45,22 @@ int _getline(char *s, int lim) {
   return i;
 }
 
-#define ALLOCSIZE 10000 /* size of available space */
-
-static char allocbuf[ALLOCSIZE]; /* storage for alloc */
-static char *allocp = allocbuf;  /* next free position */
-
-char *alloc(int n) {                        /* return pointer to n characters */
-  if (allocbuf + ALLOCSIZE - allocp >= n) { /* it fits */
-    allocp += n;
-    return allocp - n; /* old p */
-  } else {             /* not enough room */
-    return 0;
-  }
-}
 
 /* readlines:   read input lines*/
-int readlines(char *lineptr[], int maxlines) {
+int readlines(char *lineptr[], char lines[], int maxlines) {
   int len, nlines;
-  char *p, line[MAXLEN];
+  char line[MAXLEN];
+  char *lineend = lines + MAXLINES + MAXLINE;
 
   nlines = 0;
   while ((len = _getline(line, MAXLEN)) > 0) {
-    if (nlines >= maxlines || (p = alloc(len)) == NULL)
+    if (nlines >= maxlines || lines + len >= lineend)
       return -1;
     else {
       line[len - 1] = '\0'; /* delete newline */
-      strcpy(p, line);
-      lineptr[nlines++] = p;
+      strcpy(lines, line);
+      lineptr[nlines++] = lines;
+      lines += len;
     }
   }
   return nlines;
