b0a3756a7199fcef7f9a9f06ac426eb1be2a50dc
mxngls <Maximilian.e.hoenig@gmail.com>
2024-09-24 14:30:25 +0900

    Add compiler flags

--- run.sh
+++ run.sh
@@ -2,8 +2,14 @@
 
 gcc \
     -std=c90 \
+    -g3 \
     -fsanitize=undefined \
+    -fsanitize-trap \
     -Wall \
+    -Wextra \
+    -Wconversion \
+    -Wno-sign-conversion \
+    -Wdouble-promotion \
     "$1" \
     -o "./out/${1%.c}".out &&
     ./out/"${1%.c}".out "${@:2}"
