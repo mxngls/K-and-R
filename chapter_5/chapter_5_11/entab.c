055ca8ff8e4eb866a5ce7efb6aad05c8981db24e
mxngls <Maximilian.e.hoenig@gmail.com>
2024-09-29 21:37:07 +0900

    Add exercise descriptions to exercise 5-11

--- chapter_5/chapter_5_11/entab.c
+++ chapter_5/chapter_5_11/entab.c
@@ -38,6 +38,11 @@ int parse_tabs(int argc, char **argv, int ts[], int *tn) {
     return 0;
 }
 
+/*
+ * Exercise 5-11. Modify the program entab and detab (written as exercises in
+ * Chapter 1) to accept a list of tab stops as arguments. Use the default tab
+ * settings if there are no arguments.
+ */
 int main(int argc, char **argv) {
 
     int tn;                /* number tabstops */
